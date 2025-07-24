6eefc408fbe28d6629274634f9c3c972bc30f10e
Farhan R <farhanrahman675@gmail.com>
2025-07-24 14:55:54 +0100

    Task 4 Implemented

--- src/main/java/com/jpmc/midascore/foundation/TransactionService.java
+++ src/main/java/com/jpmc/midascore/foundation/TransactionService.java
@@ -4,18 +4,21 @@ import com.jpmc.midascore.entity.TransactionRecord;
 import com.jpmc.midascore.entity.UserRecord;
 import com.jpmc.midascore.repository.TransactionRecordRepository;
 import com.jpmc.midascore.repository.UserRepository;
-import org.h2.engine.User;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class TransactionService {
 
     private final UserRepository userRepository;
     private final TransactionRecordRepository transactionRepository;
+    private final RestTemplate restTemplate;
 
-    public TransactionService(UserRepository userRepository, TransactionRecordRepository transactionRepository) {
+    public TransactionService(UserRepository userRepository, TransactionRecordRepository transactionRepository, RestTemplate restTemplate) {
         this.userRepository = userRepository;
         this.transactionRepository = transactionRepository;
+        this.restTemplate = restTemplate;
     }
 
     public boolean processTransaction(Transaction transaction) {
@@ -28,6 +31,12 @@ public class TransactionService {
         sender.setBalance(sender.getBalance() - transaction.getAmount());
         recipient.setBalance(recipient.getBalance() + transaction.getAmount());
 
+        Incentive incentive = fetchIncentive(transaction);
+
+        if(incentive != null && incentive.getAmount() > 0) {
+            recipient.setBalance(recipient.getBalance() + incentive.getAmount());
+        }
+
         userRepository.save(sender);
         userRepository.save(recipient);
 
@@ -38,5 +47,18 @@ public class TransactionService {
         );
         transactionRepository.save(record);
         return true;
+    }
+
+    private Incentive fetchIncentive(Transaction transaction) {
+        try {
+            ResponseEntity<Incentive> response = restTemplate.postForEntity(
+                    "http://localhost:8080/incentive",
+                    transaction,
+                    Incentive.class
+            );
+            return response.getBody();
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
